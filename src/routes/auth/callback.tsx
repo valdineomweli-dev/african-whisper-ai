e4efe80acf72519c10619107b84c8578bfd2f66a
Mweli <valdineomweli@gmail.com>
2026-07-11 12:52:46 +0200

    Fix build - install stripe and remove duplicate callback

--- src/routes/auth/callback.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/auth/callback')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/auth/callback"!</div>
-}
